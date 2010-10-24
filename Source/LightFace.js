9fd930380fe417e9a71ee225dfa3da444adea377
David Walsh <davidwalsh83@gmail.com>
2010-10-24 00:16:28 -0500

    Lots of updates to functionality, plus test files.

--- Source/LightFace.js
+++ Source/LightFace.js
@@ -7,42 +7,49 @@
 	
 	To Do
 	-----------------------------
-		Move style to stylesheet
-		Size easing (?)
-		Redraw()
+		More explicit load system:  loadImage
+									loadRequest
+									loadIFrame
+									loadContent
+		
+		
+		
+		Better Sizing system
+		More clear show/open/close/hide
+		
+		
+		
+	Possible Enhancements
+	-----------------------------
 		IE6 - Overlay Size
-		Height, Width, Positioning + "px" stuff
+		
 */
 
 
 
-var Facebox = new Class({
+var LightFace = new Class({
 	
 	Implements: [Options,Events],
 	
 	options: {
 		
-		width: 370,
+		width: 'auto',
 		height: 'auto',
-		draggable: true,
-		submitValue: false,
-		submitFunction: false,
-		submitFocus: false,
+		draggable: false, 			//wtf?
 		title: '',
-		cancelValue: 'Cancel',
-		cancelFunction: false,
-		fadeOpacity: 1,
-		fadeDuration: 500,
-		overlayBackgroundColor: '#fff',
-		message: 'Message not specified.',
-		handler: '', //image, request, html
-		url: false,
-		method: 'get',
-		data: {},
+		buttons: [],
 		fadeDelay: 250,
+		fadeDuration: 500,
+		request: {
+			url: false
+		},
+		keys: { 
+			esc: function() { this.close(); } 
+		},
+		content: '<p>Message not specified.</p>',
+		method: 'get',
+		zIndex: 9001,
 		errorMessage: '<p>The requested file could not be found.</p>'/*,
-		onDrawStart: $empty,
-		onDrawComplete: $empty,
 		onShow: $empty,
 		onHide: $empty,
 		onClose: $empty,
@@ -58,21 +65,21 @@ var Facebox = new Class({
 	
 	initialize: function(options) {
 		this.setOptions(options);
+		this.buttons = {};
 		this.draw();
-		this.position();
-		this['handle' + (this.options.handler ? this.options.handler.capitalize() : 'Detect')]();
+		if(this.options.request.url) this.load(this.options.request.url);
+		this.attachEvents();
 	},
 	
 	draw: function() {
 		
-		this.fireEvent('drawStart');
-		
 		//create main box
 		this.box = new Element('table',{
-			'class': 'facebox',
+			'class': 'lightface',
 			styles: {
 				 position: 'absolute',
-				'z-index': 9001
+				'z-index': this.options.zIndex,
+				opacity: 0
 			},
 			morph: {
 				duration: this.options.fadeDuration
@@ -86,16 +93,15 @@ var Facebox = new Class({
 			for(var y = 0; y < len; y++) {
 				var cssClass = verts[x] + hors[y], cell = row.insertCell(y);
 				cell.className = cssClass;
+				cell.setStyle('opacity',0.4);
 				if (cssClass == 'centerCenter') {
 					this.contentBox = new Element('div',{
-						'class': 'faceboxContent',
+						'class': 'lightfaceContent',
 						styles: {
-							position: 'relative',
-							width: this.options.width,
-							height: this.options.height
+							width: this.options.width
 						}
 					});
-					cell.appendChild(this.contentBox);
+					cell.setStyle('opacity',1).appendChild(this.contentBox);
 				}
 			}
 		}
@@ -104,19 +110,9 @@ var Facebox = new Class({
 		this.overlay = new Element('div',{
 			html: '&nbsp;',
 			styles: {
-				position: 'absolute', /* move to ss */
-				left: 0, /* move to ss */
-				top: 0, /* move to ss */
-				bottom: 0, /* move to ss */
-				left: 0, /* move to ss */
-				right: 0, /* move to ss */
-				'background-color': this.options.overlayBackgroundColor, /* move to ss */
-				'background-image': 'url(fbloader.gif)', /* move to ss */
-				'background-position': 'center center', /* move to ss */
-				'background-repeat': 'no-repeat', /* move to ss */
 				opacity: 0
 			},
-			'class': 'faceboxOverlay',
+			'class': 'lightfaceOverlay',
 			tween: {
 				link: 'chain',
 				duration: this.options.fadeDuration
@@ -127,77 +123,64 @@ var Facebox = new Class({
 		if(this.options.title) {
 			this.title = new Element('h2',{
 				html: this.options.title,
-				'class': 'faceboxTitle'
+				'class': 'lightfaceTitle'
 			}).inject(this.contentBox);
-			if(this.options.draggable && Drag && Drag.Move) {
+			if(this.options.draggable && $defined(window['Drag']) && $defined(window['Drag.Move'])) {
 				new Drag.Move(this.box,{ handle: this.title });
-				this.title.addClass('faceboxDraggable');
+				this.title.addClass('lightfaceDraggable');
 			}
 		}
 		
 		//draw message box
 		if(!isNaN(this.options.height)) { this.options.height = this.options.height + 'px'; }
 		this.messageBox = new Element('div',{
-			'class': 'messageBox',
+			'class': 'lightfaceMessageBox',
+			html: this.options.content,
 			styles: {
-				height: this.options.height,
-				overflow: 'auto', /* move to ss */
-				padding: '5px 10px', /* move to ss */
-				'min-height': '50px'
+				height: this.options.height
 			}
 		}).inject(this.contentBox);
 		
-		//draw footer and buttons
-		if(this.options.submitValue || this.options.cancelValue) {
-			this.footer = new Element('div',{
-				'class': 'faceboxFooter',
-				styles: {
-					display: (this.options.submitValue || this.options.cancelValue ? 'block' : 'none')
-				}
-			}).inject(this.contentBox);
-		
-			//submit
-			if(this.options.submitValue) {
-				this.submitButton = new Element('input',{
-					type: 'button',
-					'class': 'faceboxSubmit',
-					value: this.options.submitValue,
-					events: {
-						click:this.options.submitFunction || this.close.bind(this)
-					}
-				}).inject(this.footer);
-				if(this.options.submitFocus) { this.submitButton.focus(); }
+		//button container
+		this.footer = new Element('div',{
+			'class': 'lightfaceFooter',
+			styles: {
+				display: 'none'
 			}
+		}).inject(this.contentBox);
 		
-			//cancel button
-			if(this.options.cancelValue) {
-				this.cancelButton = new Element('input',{
-					type: 'button',
-					value: this.options.cancelValue,
-					events: {
-						click: this.options.cancelFunction || this.close.bind(this)
-					}
-				}).inject(this.footer);
-			}
+		//create initial buttons
+		if(this.options.buttons.length) {
+			this.options.buttons.each(function(button) {
+				this.addButton(button.title,button.event,button.submit);
+			},this);
 		}
-		this.fireEvent('drawComplete');
 		
 		return this;
 	},
 	
-	/* put the box in the right spot */
-	position: function() {
-		var pos = window.getSize(), scroll = window.getScroll(), size = this.box.getSize();
-		this.box.setStyles({
-			left: (pos.x / 2 + scroll.x) + 'px',
-			top: (pos.y / 2 + scroll.y) + 'px',
-			'margin-left': (size.x / 2 * -1) + 'px',
-			'margin-top': (size.y / 2 * -1) + 'px'
-		});
-		return this;
+	/* adds buttons */
+	addButton: function(title,clickEvent,isSubmit) {
+		this.footer.setStyle('display','block');
+		this.buttons[title] = (new Element('input',{
+			type: 'button',
+			value: title,
+			'class': isSubmit ? 'lightfaceSubmit': '',
+			events: {
+				click: clickEvent || this.close.bind(this)
+			}
+		}).inject(this.footer));
+		
+	},
+	showButton: function(title) {
+		if(this.buttons[title]) this.buttons[title].setStyle('display','');
+	},
+	hideButton: function(title) {
+		if(this.buttons[title]) this.buttons[title].setStyle('display','none');
 	},
 	
 	show: function() {
+		this._resize();
 		this.box.morph({ opacity: 1 });
 		this.fireEvent('show');
 		return this;
@@ -212,13 +195,21 @@ var Facebox = new Class({
 	close: function() {
 		this.box.setStyle('opacity',0);
 		this.fireEvent('close');
+		this.detachEvents();
+		return this;
+	},
+	
+	open: function() {
+		this._resize();
+		thix.box.setStyle('opacity',1);
+		this.fireEvent('open');
+		this.box.setAttribute('tabIndex',0).focus();
 		return this;
 	},
 	
 	/* shows and hides overlay */
 	fade: function(fade) {
-		//this.overlay.fade(this.options.fadeOpacity);
-		this.overlay.setStyle('opacity',fade || this.options.fadeOpacity);
+		this.overlay.setStyle('opacity',fade || 1);
 		this.fireEvent('fade');
 		return this;
 	},
@@ -231,78 +222,156 @@ var Facebox = new Class({
 		return this;
 	},
 	
-	/* handlers */
-	handleImage: function() {
+	/*
+		LOADING AND SETTING OF CONTENT
+	*/
+	
+	load: function(url) {
+		
+		if(typeof url != 'string') url = url.url;
+		
+		if(url && ['png','jpg','jpeg','gif'].contains(url.substr(url.lastIndexOf('.') + 1,url.length))) {
+			this.handleImage(url);
+		}
+		else {
+			this.handleRequest(url);
+		}
+		return this;
+	},
+	
+	setContent: function(content,title) {
+		this.messageBox.set('html',content);
+		if(title) this.title.set('html',title);
+		this._resize();
+		this.fireEvent('complete');
+		return this;
+	},
+	
+	handleImage: function(url) {
+		if(!this.image) {
+			this.messageBox.set('html','');
+			this.image = new Element('img',{
+				events: {
+					click: function() {
+						this.hide();
+					}.bind(this),
+					error: function() {
+						this.unfade();
+						this.messageBox.set('html',this.options.errorMessage);
+						this.image.dispose();
+						this.fireEvent('complete');
+					}.bind(this),
+					load: function() {
+						this._resize.bind(this,[true]).delay(10);
+						this.image.setStyle('display','');
+						this.unfade();
+						this.fireEvent('complete');
+					}.bind(this)
+				},
+				styles: {
+					display: 'none'
+				}
+			});
+		}
 		this.fade();
-		var img = new Element('img',{
-			alt: this.title ? this.title.get('html') : this.options.url,
-			events: {
-				error: function() {
-					this.messageBox.set('html',this.options.errorMessage);
-					img.dispose();
-				}.bind(this),
-				load: function() {
-					img.setStyle('display','');
-					this.unfade();
-					if(!this.footer) {
-						img.setStyle('cursor','pointer').addEvent('click',this.close.bind(this));
-					}
-				}.bind(this)
-			},
-			styles: {
-				display: 'none'
-			}
-		});
-		img.set('src',this.options.url).inject(this.messageBox); //for ie
-		this.fireEvent('complete');
+		this.image.set('src',url).inject(this.messageBox); //for ie
 		return this;
 	},
 	
-	handleRequest: function() {
-		var self = this;
-		new Request({
-			url: self.options.url,
-			method: self.options.method,
-			data: self.options.data,
+	handleRequest: function(url) {
+		this.options.request.url = url;
+		var props = $extend({
 			onRequest: function() {
-				self.fade();
-				self.fireEvent('request');
-			},
+				this.fade();
+				this.fireEvent('request');
+			}.bind(this),
 			onSuccess: function(response) {
-				self.messageBox.set('html',response);
-				self.fireEvent('success');
-			},
+				this.messageBox.set('html',response);
+				this.fireEvent('success');
+			}.bind(this),
 			onFailure: function() {
-				self.messageBox.set('html',self.options.errorMessage);
-				self.fireEvent('failure');
-			},
+				this.messageBox.set('html',this.options.errorMessage);
+				this.fireEvent('failure');
+			}.bind(this),
 			onComplete: function() {
-				self.unfade();
-				self.fireEvent('complete');
+				this._resize();
+				this.unfade();
+				this.fireEvent('complete');
+			}.bind(this)
+		},this.options.request);
+		
+		console.log(props);
+		
+		new Request(props).send();
+		return this;
+	},
+	
+	/*
+		Keyboard and Window events
+	*/
+	attachEvents: function() {
+		
+		this.keyEvent = function(e){
+			if(this.options.keys[e.key]) {
+				this.options.keys[e.key].call(this);
 			}
-		}).send();
+		}.bind(this);
+		document.addEvent('keyup',this.keyEvent);
+		
+		this.resizeEvents = function() {
+			this._resize();
+		}.bind(this);
+		window.addEvent('resize',this.resizeEvents);
+		
 		return this;
 	},
 	
-	handleHTML: function() {
-		this.messageBox.set('html',this.options.message);
-		this.fireEvent('complete');
+	detachEvents: function() {
+		document.removeEvent('keyup',this.keyEvent);
+		document.removeEvent('resize',this.resizeEvents);
 		return this;
 	},
 	
-	handleDetect: function() {
-		//fire handler
-		if(this.options.url && ['png','jpg','gif'].contains(this.options.url.substr(this.options.url.lastIndexOf('.') + 1,this.options.url.length))) {
-			this.handleImage();
-		}
-		else if(this.options.url && !this.options.url.contains(' ')) { //AJAX - lame space checker...
-			this.handleRequest();
-		}
-		else { // string content
-			this.handleHTML();
-		}
+	/*
+		Resizing, poisitioning
+	*/
+	position: function() {
+		var windowSize = window.getSize(), 
+			scrollSize = window.getScroll(), 
+			boxSize = this.box.getSize();
+		this.box.setStyles({
+			left: scrollSize.x + ((windowSize.x - boxSize.x) / 2),
+			top: scrollSize.y + ((windowSize.y - boxSize.y) / 2)
+		});
 		return this;
+	},
+	
+	_calculateHeight: function(suggested) {
+		var height = suggested, threshhold = 110;
+		if(suggested == 'auto') {
+			//get the height of the content box
+			var maxHeight = window.getSize().y - threshhold;
+			if(this.contentBox.getSize().y > maxHeight) {
+				return maxHeight;
+			}
+		}
+		return height;
+	},
+	
+	_resize: function(constrainImage) {
+		this.messageBox.setStyle('height',this._calculateHeight(this.options.height));
+		if(constrainImage) {
+			//get image dimensions
+			var boxSize = this.messageBox.getSize().y;
+			if(this.image.getSize().y > boxSize) {
+				this.image.setStyle('height',boxSize - 30);
+			}
+		}
+		this.position();
+	},
+	
+	toElement: function () {
+		return this.box;
 	}
 	
-});	
-Facebox.alias('handleHTML','handleHtml');
+});
