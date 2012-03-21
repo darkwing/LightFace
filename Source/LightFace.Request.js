28bd90f1d0e8c07b9080ac466df2932ee3959005
David Walsh <dwalsh@sitepen.com>
2012-03-21 08:33:33 -0500

    Reformatting quotes and arguments.  Updating for Moo 1.4 with overlay visibility

--- Source/LightFace.Request.js
+++ Source/LightFace.Request.js
@@ -9,7 +9,7 @@ license:
   - MIT-style license
 
 requires:
-  core/1.2.1:   '*'
+  core/1.2.1:   "*"
 
 provides:
   - LightFace.Request
@@ -18,7 +18,7 @@ provides:
 LightFace.Request = new Class({
 	Extends: LightFace,
 	options: {
-		url: '',
+		url: "",
 		request: {
 			url: false
 		}
@@ -27,30 +27,30 @@ LightFace.Request = new Class({
 		this.parent(options);
 		if(this.options.url) this.load();
 	},
-	load: function(url,title) {
+	load: function(url, title) {
 		var props = (Object.append || $extend)({
 			onRequest: function() {
 				this.fade();
-				this.fireEvent('request');
+				this.fireEvent("request");
 			}.bind(this),
 			onSuccess: function(response) {
-				this.messageBox.set('html',response);
-				this.fireEvent('success');
+				this.messageBox.set("html", response);
+				this.fireEvent("success");
 			}.bind(this),
 			onFailure: function() {
-				this.messageBox.set('html',this.options.errorMessage);
-				this.fireEvent('failure');
+				this.messageBox.set("html", this.options.errorMessage);
+				this.fireEvent("failure");
 			}.bind(this),
 			onComplete: function() {
 				this._resize();
 				this._ie6Size();
-				this.messageBox.setStyle('opacity',1);
+				this.messageBox.setStyle("opacity", 1);
 				this.unfade();
-				this.fireEvent('complete');
+				this.fireEvent("complete");
 			}.bind(this)
 		},this.options.request);
 		
-		if(title && this.title) this.title.set('html',title);
+		if(title && this.title) this.title.set("html", title);
 		if(!props.url) props.url = url || this.options.url;
 		
 		new Request(props).send();
